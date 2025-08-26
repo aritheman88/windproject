3dfd1ab9f83e49d1e72dae619230108ffd2bbcc5
aritheman88 <65540935+aritheman88@users.noreply.github.com>
2025-08-26 06:45:42 +0300

    Delete wind_docx.py

--- wind_docx.py
+++
@@ -1,8 +0,0 @@
-import docx, os, time
-from datetime import date, datetime
-
-today = date.today()
-print("Date: ", today)
-now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
-print("Current Time =", current_time[:-3])
